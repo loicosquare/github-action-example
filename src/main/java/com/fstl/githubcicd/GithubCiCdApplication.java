352f941d9c57ca959cb19b3061daac3163f114e0
Loic Sanou <loicsan07@gmail.com>
2023-11-22 20:48:25 +0100

    UPdate

--- src/main/java/com/fstl/githubcicd/GithubCiCdApplication.java
+++ src/main/java/com/fstl/githubcicd/GithubCiCdApplication.java
@@ -2,10 +2,18 @@ package com.fstl.githubcicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 @RestController
 public class GithubCiCdApplication {
+
+    @GetMapping("/welcome")
+    public String Welcome() {
+        return "Welcome to FSTL";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GithubCiCdApplication.class, args);
